03222a84a4215665aad740ef5452cea5eb434d49
tuyuaner <tuyuaner@gmail.com>
2017-07-14 01:10:53 -0400

    arcade

---
+++ js/app.js
@@ -0,0 +1,139 @@
+// superclass Character
+var Character = function(sprite, x, y, speed){
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+};
+
+Character.prototype.render = function(dt){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+//subclass Enemy and player
+// Enemies our player must avoid
+var Enemy = function(sprite, x, y, speed) {
+    // Variables applied to each of our instances go here,
+    Character.call(this, sprite, x, y, speed);
+
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+};
+
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Enemy;
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if (this.x < 500){
+        this.x += this.speed * dt;    
+
+    }else{
+        this.x = -100;
+        this.y = Math.random() * 184 + 50;
+        if (allEnemies.length < 5){
+            var enemy = new Enemy('images/char-horn-girl.png', -100, Math.random() * 184 + 80, Math.random() * 256 + 40);
+            allEnemies.push(enemy);
+        }else{
+            delete allEnemies[this];
+            console.log(this);
+        }
+    }
+    collision();
+};
+
+var collision = function(){
+    for (var i = 0; i < allEnemies.length; i++){
+        if (Math.abs(player.x - allEnemies[i].x) <= 40 && Math.abs(player.y - allEnemies[i].y) <= 40){
+            player.reset();
+        }
+    }
+
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(sprite, x, y, speed){
+    Character.call(this, sprite, x, y, speed);
+};
+
+Player.prototype = Object.create(Character.prototype);
+Player.constructor = Player;
+
+// Player.prototype.update= function(){
+
+// };
+
+Player.prototype.handleInput = function(input){
+    if (input === "left"){
+        if (this.x > 20){
+            this.x -= this.speed;
+        }
+        
+    }
+    else if (input === "up"){
+        if (this.y > 20){
+            this.y -= this.speed;
+        }else{
+            this.y -= this.speed;
+            this.reset();
+        }
+    }
+    else if (input === "right"){
+        if (this.x < 400){
+            this.x += this.speed;
+        }
+    }
+    else{
+        if (this.y < 380){
+            this.y += this.speed;
+        }
+       
+    }
+};
+
+Player.prototype.reset = function(){
+    this.x = 200;
+    this.y = 380;
+};
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+for (var i = 0; i < 2; i++){
+    var enemy = new Enemy('images/char-horn-girl.png', -100, Math.random() * 184 + 80, Math.random() * 256 + 40);
+    allEnemies.push(enemy);
+};
+
+
+
+// Place the player object in a variable called player
+var player = new Player("images/char-boy.png", 200, 380, 60);
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
